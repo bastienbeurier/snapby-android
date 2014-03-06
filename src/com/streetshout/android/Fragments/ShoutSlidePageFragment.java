f724935d1acf1fb9c738eefc4b8a4a2b3f20a34c
Bastien Beurier <bastienbeurier@gmail.com>
2014-03-06 12:26:06 +0100

    Reredesign feed

--- src/com/streetshout/android/Fragments/ShoutSlidePageFragment.java
+++ src/com/streetshout/android/Fragments/ShoutSlidePageFragment.java
@@ -3,7 +3,6 @@ package com.streetshout.android.fragments;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,11 +42,13 @@ public class ShoutSlidePageFragment extends Fragment {
 
     private TextView usernameView = null;
 
-    private View coloredBar = null;
-
-    private View coloredMetaDataContainer = null;
+    private View coloredTimeDistanceContainer = null;
 
     private LinearLayout coloredButtonContainer = null;
+
+    private LinearLayout coloredLikeCountButton = null;
+
+    private LinearLayout coloredCommentCountButton = null;
 
     public static ShoutSlidePageFragment newInstance(Shout shout) {
         ShoutSlidePageFragment shoutSlidePageFragment = new ShoutSlidePageFragment();
@@ -73,14 +74,16 @@ public class ShoutSlidePageFragment extends Fragment {
         commentCountView = (TextView) rootView.findViewById(R.id.explore_shout_comment_count);
         usernameView = (TextView) rootView.findViewById(R.id.explore_shout_username);
         descriptionView = (TextView) rootView.findViewById(R.id.explore_shout_description);
-        coloredBar = rootView.findViewById(R.id.explore_shout_colored_bar);
-        coloredMetaDataContainer = rootView.findViewById(R.id.explore_shout_colored_meta_data_container);
+        coloredTimeDistanceContainer = rootView.findViewById(R.id.explore_shout_colored_time_distance_container);
         coloredButtonContainer = (LinearLayout) rootView.findViewById(R.id.explore_shout_colored_button_container);
+        coloredLikeCountButton = (LinearLayout) rootView.findViewById(R.id.explore_shout_like_count_button);
+        coloredCommentCountButton = (LinearLayout) rootView.findViewById(R.id.explore_shout_comment_count_button);
 
-        likeCountView.setText(Integer.toString(shout.likeCount));
-        commentCountView.setText(Integer.toString(shout.commentCount));
 
         aq.id(imageView).image(shout.image + "--400", true, false, 0, 0, null, AQuery.FADE_IN);
+
+        setLikeCountUI(shout.likeCount);
+        setCommentCountUI(shout.commentCount);
 
         String[] ageStrings = TimeUtils.shoutAgeToShortStrings(TimeUtils.getShoutAge(shout.created));
         ageView.setText(ageStrings[0] + ageStrings[1]);
@@ -98,17 +101,11 @@ public class ShoutSlidePageFragment extends Fragment {
         }
 
         if (shout.id == SessionUtils.getCurrentUser(getActivity()).id) {
-            coloredBar.setBackgroundColor(getResources().getColor(R.color.myShoutPurple));
-            coloredButtonContainer.setBackgroundColor(getResources().getColor(R.color.myShoutPurple));
-            ImageUtils.setBackground(getActivity(), coloredMetaDataContainer, R.drawable.my_shout_meta_info);
+            updateShoutUI(R.color.myShoutPurple);
         } else if (shout.anonymous) {
-            coloredBar.setBackgroundColor(getResources().getColor(R.color.anonymousGrey));
-            coloredButtonContainer.setBackgroundColor(getResources().getColor(R.color.anonymousGrey));
-            ImageUtils.setBackground(getActivity(), coloredMetaDataContainer, R.drawable.anonymous_shout_meta_info);
+            updateShoutUI(R.color.anonymousGrey);
         } else {
-            coloredBar.setBackgroundColor(getResources().getColor(R.color.publicYellow));
-            coloredButtonContainer.setBackgroundColor(getResources().getColor(R.color.publicYellow));
-            ImageUtils.setBackground(getActivity(), coloredMetaDataContainer, R.drawable.public_shout_meta_info);
+            updateShoutUI(R.color.publicYellow);
         }
 
         if (shout.anonymous) {
@@ -128,5 +125,43 @@ public class ShoutSlidePageFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    private void updateShoutUI(int color) {
+        coloredButtonContainer.setBackgroundColor(getResources().getColor(color));
+
+        switch(color){
+            case R.color.myShoutPurple:
+                ImageUtils.setBackground(getActivity(), coloredTimeDistanceContainer, R.drawable.my_shout_meta_info);
+                ImageUtils.setBackground(getActivity(), coloredLikeCountButton, R.drawable.my_shout_count_button_selector);
+                ImageUtils.setBackground(getActivity(), coloredCommentCountButton, R.drawable.my_shout_count_button_selector);
+                return;
+            case R.color.anonymousGrey:
+                ImageUtils.setBackground(getActivity(), coloredTimeDistanceContainer, R.drawable.anonymous_shout_meta_info);
+                ImageUtils.setBackground(getActivity(), coloredLikeCountButton, R.drawable.anonymous_count_button_selector);
+                ImageUtils.setBackground(getActivity(), coloredCommentCountButton, R.drawable.anonymous_count_button_selector);
+                return;
+            case R.color.publicYellow:
+                ImageUtils.setBackground(getActivity(), coloredTimeDistanceContainer, R.drawable.public_shout_meta_info);
+                ImageUtils.setBackground(getActivity(), coloredLikeCountButton, R.drawable.public_shout_count_button_selector);
+                ImageUtils.setBackground(getActivity(), coloredCommentCountButton, R.drawable.public_shout_count_button_selector);
+                return;
+        }
+    }
+
+    private void setLikeCountUI(int count) {
+        if (count > 1) {
+            likeCountView.setText(Integer.toString(shout.likeCount) + " " + getString(R.string.likes));
+        } else {
+            likeCountView.setText(Integer.toString(shout.likeCount) + " " + getString(R.string.like));
+        }
+    }
+
+    private void setCommentCountUI(int count) {
+        if (count > 1) {
+            commentCountView.setText(Integer.toString(shout.commentCount) + " " + getString(R.string.comments));
+        } else {
+            commentCountView.setText(Integer.toString(shout.commentCount) + " " + getString(R.string.comment));
+        }
     }
 }
